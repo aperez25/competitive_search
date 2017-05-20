ea090d8c25b4db7c938589188d195bd3eee931b4
aperez25 <alice.perez25@gmail.com>
2017-05-20 12:44:24 -0700

    finished minimax & minimaxAB

--- src/minimax.js
+++ src/minimax.js
@@ -1,4 +1,3 @@
-
 /*
 The function "makeMove" is already written for you.
 You do not need to modify it, but you should read it.
@@ -31,9 +30,6 @@ const makeMove = (state) => {
 	// It autmatically switches the player whose
 	// move it is, adds the piece to the board, etc.
 	//
-	// Note that state is immutable; invoking state.move
-	// does NOT change the original state, but
-	// returns a new one.
 	const newState = state.move(allLegalMoves[0]);
 
 	// The following is the guts of the make-move function.
@@ -53,7 +49,7 @@ const makeMove = (state) => {
 		// minimax call unchanged.
 
 		const stateValue = minimax(potentialState, depth, playerMoving);
-		//const stateValue = minimaxAlphaBetaWrapper(potentialState, depth, playerMoving)
+		// const stateValue = minimaxAlphaBetaWrapper (potentialState, depth, playerMoving)
 
 		if (stateValue > bestMoveValue || bestMoveValue === null){
 			bestMoveIndex = i;
@@ -66,71 +62,72 @@ const makeMove = (state) => {
 }
 
 const heuristic = (state, maximizingPlayer) => {
-
-  const minimizingPlayer = maximizingPlayer === 'x' ? 'o' : 'x';
-
-	const playerScore = (player) =>
-		[2, 3, 4].reduce((acc, val) => {
-			return acc + (state.numLines(val, player) * val)
-		}, 0)
-
-	return playerScore(maximizingPlayer) - playerScore(minimizingPlayer)
+  const minimizingPlayer = (maximizingPlayer === 'x') ? 'o' : 'x';
+	// return a score with weights based on line length
+	const playerScore = player =>
+	// array of possible line lengths
+		[2, 3, 4].reduce((acc, lineLength) =>
+			acc + (state.numLines(lineLength, player) * lineLength), 0);
+	return playerScore(maximizingPlayer) - playerScore(minimizingPlayer);
 }
 
-/*
-
-Input: state, depth, maximizingPlayer.  The state is
-an instance of a state object.  The depth is an integer greater than zero; when it is zero, the minimax function should return the value of the heuristic function.
-
-Output: Returns a number evaluating the state, just
-like heuristic does.
-
-You'll need to use state.nextStates(), which returns
-a list of possible successor states to the state passed in as an argument.
-
-You'll also probably need to use state.nextMovePlayer,
-which returns whether the next moving player is 'x' or 'o', to see if you are maximizing or minimizing.
-*/
 const minimax = (state, depth, maximizingPlayer) => {
-	let minimizingPlayer = (maximizingPlayer === 'x') ? 'o' : 'x',
-	bestScore = 0,
-	possibleStates = state.nextStates(),
-	currentPlayer = state.nextMovePlayer;
-
-	if (depth === 0 || !possibleStates.length) bestScore += heuristic(state, maximizingPlayer);
-	else if (currentPlayer === maximizingPlayer) {
-
+	let possibleStates = state.nextStates(),
+		currentPlayer = state.nextMovePlayer;
+	// check for base case
+	if (depth === 0 || !possibleStates.length) return heuristic(state, maximizingPlayer);
+	 else {
+		// find the next possible moves
+		let possibleStatevalues = possibleStates.map(nextState => minimax(nextState, depth - 1, maximizingPlayer))
+		// check the currentPlayer and find score accordingly
+			if (maximizingPlayer === currentPlayer) return Math.max.apply(null, possibleStatevalues);
+			else return Math.min.apply(null, possibleStatevalues);
 	}
-	else {
-
-	}
-	return bestScore;
 }
-
 
 /* minimaxAlphaBetaWrapper is a pre-written function, but it will not work
    unless you fill in minimaxAB within it.
-
    It is called with the same values with which minimax itself is called.*/
 const minimaxAlphaBetaWrapper = (state, depth, maximizingPlayer) => {
-
+	let possibleStates = state.nextStates(),
+	currentPlayer = state.nextMovePlayer;
     /*
-    You will need to write minimaxAB for the extra credit.
-    Input: state and depth are as they are before.  (Maximizing player
-    is closed over from the parent function.)
+    Alpha is the BEST value currently guaranteed to the maximizing player, if they play well, no matter what the minimizing player does; this is why it is a very low number to start with.
 
-    Alpha is the BEST value currently guaranteed to the maximizing
-    player, if they play well, no matter what the minimizing player
-    does; this is why it is a very low number to start with.
-
-    Beta is the BEST value currently guaranteed to the minimizing
-    player, if they play well, no matter what the maximizing player
-    does; this is why it is a very high value to start with.
+    Beta is the BEST value currently guaranteed to the minimizing player, if they play well, no matter what the maximizing player does; this is why it is a very high value to start with.
 	*/
 	const minimaxAB = (state, depth, alpha, beta) => {
-	}
 
-	return minimaxAB(state, depth, -100000,100000);
+		if (depth === 0 || !possibleStates.length) return heuristic(state, maximizingPlayer);
+		else {
+			if (maximizingPlayer === currentPlayer) {
+				let bestMoveSoFar = -100000;
+				for (var i = 0; i < possibleStates.length; i++) {
+					let nextState = possibleStates[i];
+					let valueOfState = minimaxAB(nextState, depth - 1, alpha, beta);
+					alpha = Math.max(alpha, valueOfState);
+					bestMoveSoFar = Math.max(bestMoveSoFar, valueOfState);
+						if (alpha > beta) {
+							return bestMoveSoFar;
+						}
+				}
+			return bestMoveSoFar;
+		} else {
+				let bestMoveSoFar = 100000;
+				for (var i = 0; i < possibleStates.length; i++) {
+					let nextState = possibleStates[i];
+					let valueOfState = minimaxAB(nextState, depth - 1, alpha, beta);
+					beta = Math.min(beta, valueOfState);
+					bestMoveSoFar = Math.min(bestMoveSoFar, valueOfState)
+						if (alpha > beta) {
+							return bestMoveSoFar;
+						}
+				}
+			return bestMoveSoFar;
+		}
+	}
+	}
+	return minimaxAB(state, depth, alpha, beta);
 }
 
-module.exports = {makeMove, minimax, heuristic};
+module.exports = { makeMove, minimax, heuristic };
--- src/minimax_specs.js
+++ src/minimax_specs.js
@@ -166,7 +166,7 @@ describe('Testing some basic functions in the minimax evaluation function', func
 				s = s.move(Math.floor(Math.random()*7));
 			}
 			let val = minimax(s, Math.floor(Math.random()*2), 'x');
-			expect(typeof val == 'number').to.equal(true);;
+			expect(typeof val == 'number').to.equal(true);
 		}
 
 	});
